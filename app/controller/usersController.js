f936212e5117c32a34511a2468e1116ca981e5b3
Bernardo Amaral <b.aoliveira@tjrs.jus.br>
2019-02-22 09:37:43 -0300

    config file for storage the token

--- app/controller/usersController.js
+++ app/controller/usersController.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
-const User = require('../model/usersModel.js');
+const crypto = require('crypto');
+const User = require('../model/usersModel');
+const config = require('../config');
 
 class UserController {
   static listAll(req, res) {
@@ -10,7 +12,8 @@ class UserController {
   }
 
   static authUser(req, res) {
-    const token = jwt.sign({ foo: 'bar' }, 'shhhhh', { expiresIn: '24h' });
+    const user = new User(req.body);
+    const token = jwt.sign({ name: user.name, email: user.email }, config.jwtSecret, { expiresIn: '24h' });
     res.json(token);
   }
 
@@ -33,6 +36,7 @@ class UserController {
     if (!newUser.name && !newUser.email && !newUser.password) {
       res.status(400).send({ error: true, message: 'Please provide user required fields' });
     } else {
+      newUser.password = crypto.createHash('md5').update(newUser.password).digest('hex');
       User.createUser(newUser, (err, user) => {
         if (err) { res.send(err); }
         res.json(user);
