3a68ee01e7a801385eba910b082ac89996e52ac9
Bernardo Amaral <b.aoliveira@tjrs.jus.br>
2019-04-16 18:22:13 -0300

    Adjusts database config

--- app/model/database.js
+++ app/model/database.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const config = require('../../database');
+const config = require('../database');
 
 const connectionString = `postgres://${config.dev.user}:${config.dev.password}@${config.dev.host}:${config.dev.port}/${config.dev.database}`;
 
