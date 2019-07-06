c2c2daa48f085f65811eee841f5b9d261b1c7157
Bernardo Amaral <bernardo.amaral85@gmail.com>
2019-07-06 11:33:23 -0300

    adjusts on cron module

--- app/cron.js
+++ app/cron.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 // const cron = require('node-cron');
-const spiderModel = require('./app/model/spiderModel');
-const MercadoLivreSpider = require('./app/spiders/MercadoLivreSpider');
+const spiderModel = require('./model/spiderModel');
+const MercadoLivreSpider = require('./spiders/MercadoLivreSpider');
 
 // cron.schedule('*/1 * * * *', () => {
 const mercadoLivreSpider = new MercadoLivreSpider();
@@ -9,21 +9,21 @@ const mercadoLivreSpider = new MercadoLivreSpider();
 const userId = 1;
 
 spiderModel.getAllTermsByUserId(userId, (response) => {
-    response.map(termItem, () => {
-        mercadoLivreSpider.getWebsiteContent(termItem.value).then((data) => {
-            termData = {
-                user_id: userId,
-                product_title: data.product_title,
-                product_url: data.product_url,
-                product_price: data.product_price,
-                product_shipping: data.product_shipping,
-                product_image: data.product_image,
-            }
-            spiderModel.insertData(termData, (result) => {
-                console.log('New registry created')
-            });
-            console.log(response, data, termItem);
-        });
+  response.map((termItem) => {
+    mercadoLivreSpider.getWebsiteContent(termItem.value).then((data) => {
+      const termData = {
+        user_id: userId,
+        product_title: data.product_title,
+        product_url: data.product_url,
+        product_price: data.product_price,
+        product_shipping: data.product_shipping,
+        product_image: data.product_image,
+      };
+      spiderModel.insertData(termData, (result) => {
+        console.log('New registry created', result);
+      });
+      console.log(response, data, termItem);
     });
+  });
 });
 // });
