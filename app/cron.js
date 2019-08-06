9e33d8d3ab249224b70da0f443a886926ecbbb7f
Bernardo Amaral <bernardo.oliveira@cwi.com.br>
2019-08-06 14:00:33 -0300

    eslint fix

--- app/cron.js
+++ app/cron.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* eslint-disable */
 // const cron = require('node-cron');
 const spiderModel = require('./model/spiderModel');
 const MercadoLivreSpider = require('./spiders/MercadoLivreSpider');
@@ -28,31 +28,7 @@ spiderModel.getAllTermsByUserId(userId).then((userTherms) => {
         });
       });
 
-
-      // console.log(termData);
     });
   });
 });
 
-
-// eslint-disable-next-line array-callback-return
-// if (response) {
-//   response.map((termItem) => {
-//     mercadoLivreSpider.getWebsiteContent(termItem.value).then((data) => {
-//       const termData = {
-//         user_id: 1,
-//         product_title: data.product_title,
-//         product_url: data.product_url,
-//         product_price: data.product_price,
-//         product_shipping: data.product_shipping,
-//         product_image: data.product_image,
-//       };
-//       spiderModel.insertData(termData, (result) => {
-//         console.log('New registry created', result);
-//       });
-//       console.log(response, data, termItem);
-//     });
-//   });
-// }
-// });
-// });
