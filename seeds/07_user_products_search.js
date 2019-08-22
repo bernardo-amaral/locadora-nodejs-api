0b68dab6c989518896f99ca7f60cfdf8b8263067
Bernardo Amaral <bernardo.oliveira@cwi.com.br>
2019-08-22 14:29:03 -0300

    Added insert endpoint to therms

--- seeds/07_user_products_search.js
+++ seeds/07_user_products_search.js
@@ -3,6 +3,9 @@
 exports.seed = function (knex) {
   return knex('user_spiders_terms').del()
     .then(() => knex('user_spiders_terms').insert([
-      { user_id: 1, value: 'Booldstained-Nintendo-Switch' },
+      { user_id: 1, value: 'Bloodstained-midia-fisica-Nintendo-Switch' },
+      { user_id: 1, value: 'crash-team-racing-midia-fisica-Nintendo-Switch' },
+      { user_id: 1, value: 'shantae-midia-fisica-Nintendo-Switch' },
+      { user_id: 1, value: 'wonder-boy-midia-fisica-Nintendo-Switch' },
     ]));
 };
