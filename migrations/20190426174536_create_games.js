ce2ba041955bbb98e741b55b05a25d5ea6b4c456
Bernardo Amaral <bernardo.oliveira@cwi.com.br>
2019-10-08 17:43:00 -0300

    Add new column to amiibo

--- migrations/20190426174536_create_games.js
+++ migrations/20190426174536_create_games.js
@@ -9,6 +9,7 @@ exports.up = function (knex) {
     table.string('genre');
     table.integer('console_id').unsigned();
     table.string('release_year');
+    table.string('amiibo_series');
     table.string('cover_picture');
     table.string('mode').defaultTo('Single-player');
     table.unique(['title', 'console_id']);
