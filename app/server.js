a3a2fb187c78f713ab596a1d49af9a7794231dec
Bernardo Amaral <bernardo.amaral85@gmail.com>
2020-09-25 14:21:40 -0300

    Adjusts on unit tests

--- app/server.js
+++ app/server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
-
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`API server started on: ${port}`));
 app.use(bodyParser.urlencoded({ extended: true }));
