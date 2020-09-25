ec1230299a06c66c8ba5ed8f9a29e8f12e52ac37
Bernardo Amaral <bernardo.amaral85@gmail.com>
2020-09-25 14:06:38 -0300

    Adjusts on unit tests

---
+++ test/unit/auth.spec.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe('Auth Tests', () => {
+  describe('#login', () => {
+    it('Shoult try login with an user and return success', () => {
+      expect(true);
+    });
+  });
+});
